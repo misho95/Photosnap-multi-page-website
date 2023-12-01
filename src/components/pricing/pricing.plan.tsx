13e22698b5af29aaa1e855038853045c1b686c1a
Misho <chapidze.misho95@gmail.com>
2023-12-01 14:16:07 +0400

    fxd

--- src/components/pricing/pricing.plan.tsx
+++ src/components/pricing/pricing.plan.tsx
@@ -18,8 +18,10 @@ const PricingPlan = ({
   return (
     <div
       className={`${
-        active === title ? "lg:h-[470px]" : " lg:h-[407px] bg-[#F5F5F5]"
-      } w-full lg:w-[350px] h-[407px] sm:h-[270px] bg-black relative font-DMSans flex flex-col gap-[20px] justify-center items-center sm:items-start lg:items-center p-[40px]`}
+        active === title
+          ? "lg:h-[470px] bg-black"
+          : " lg:h-[407px] bg-[#F5F5F5]"
+      } w-full lg:w-[350px] h-[407px] sm:h-[270px]  relative font-DMSans flex flex-col gap-[20px] justify-center items-center sm:items-start lg:items-center p-[40px]`}
     >
       {active === title && (
         <div className="h-[6px] w-full absolute top-0 left-0 bg-gradient-to-t from-[#FFC593] via-[#BC7198] to-[#5A77FF]" />
