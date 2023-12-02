02b76f149cf8e1d4cd625db8b399de0520a00039
Misho <chapidze.misho95@gmail.com>
2023-12-02 13:15:53 +0400

    fxd

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,8 @@
 import { animated, useSpring } from "@react-spring/web";
 import StoriesComponent from "../components/index/stories.component";
+import StorieCard from "../components/stories/storie.card";
+import { useEffect, useRef, useState } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const IndexPage = () => {
   const data = [
@@ -35,6 +38,48 @@ const IndexPage = () => {
     },
   ];
 
+  const data2 = [
+    {
+      id: 1,
+      date: "April 16th 2020",
+      title: "The Mountains",
+      author: "John Appleseed",
+      img: "/assets/stories/desktop/mountains.jpg",
+    },
+    {
+      id: 2,
+      date: "April 14th 2020",
+      title: "Sunset Cityscapes",
+      author: "Benjamin Cruz",
+      img: "/assets/stories/desktop/cityscapes.jpg",
+    },
+    {
+      id: 3,
+      date: "April 11th 2020",
+      title: "18 Days Voyage",
+      author: "Alexei Borodin",
+      img: "/assets/stories/desktop/18-days-voyage.jpg",
+    },
+    {
+      id: 4,
+      date: "April 9th 2020",
+      title: "Architecturals",
+      author: "Samantha Brooke",
+      img: "/assets/stories/desktop/architecturals.jpg",
+    },
+  ];
+
+  const containerRef = useRef<HTMLInputElement>(null);
+  const windowSize = useWindowSize();
+  const [divSize, setDivSize] = useState<number>(0);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const size = containerRef.current.offsetWidth;
+      setDivSize(+size);
+    }
+  }, [windowSize]);
+
   const animatedPage = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
@@ -61,6 +106,20 @@ const IndexPage = () => {
             />
           );
         })}
+        <div className="flex w-full flex-wrap" ref={containerRef}>
+          {data2.map((d) => {
+            return (
+              <StorieCard
+                key={d.id}
+                date={d.date}
+                title={d.title}
+                author={d.author}
+                img={d.img}
+                divSize={divSize}
+              />
+            );
+          })}
+        </div>
       </animated.section>
     </main>
   );
